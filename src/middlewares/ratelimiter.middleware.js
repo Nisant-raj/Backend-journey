dea0d6a3e701412735ff2b39722b88452c51148f
Nishant Raj <nishant.r@sapphirus.in>
2026-01-21 11:28:26 +0530

    Day 11: LLD - Rate limiter design & implementation

--- src/middlewares/ratelimiter.middleware.js
+++ src/middlewares/ratelimiter.middleware.js
@@ -1,10 +1,25 @@
-const rateLimit= require("express-rate-limit") ;
+// const rateLimit= require("express-rate-limit") ;
 
- const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
-  message: 'Too many requests, please try again later'
-});
+//  const apiLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100,
+//   message: 'Too many requests, please try again later'
+// });
 
 
-module.exports =apiLimiter;
+const TokenBucket = require('../services/tokenBucket.service.js');
+
+const bucket = new TokenBucket(100, 1); // 100 requests, 1 req/sec refill
+
+ const rateLimiter = (req, res, next) => {
+  if (!bucket.allowRequest()) {
+    return res.status(429).json({
+      message: 'Too many requests. Please try again later.'
+    });
+  }
+  next();
+};
+
+
+
+module.exports =rateLimiter;
